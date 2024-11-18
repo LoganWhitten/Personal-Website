a351b5da9cc261350fabc713cdc67ffe81ed0ca6
lights9999 <32558210+LoganWhitten@users.noreply.github.com>
2024-11-18 10:16:40 -0500

    update netlify

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,12 +1,8 @@
 import { defineConfig } from 'astro/config';
-
-import node from "@astrojs/node";
+import netlify from '@astrojs/netlify';
 
 // https://astro.build/config
 export default defineConfig({
-  integrations: [],
-  output: "server",
-  adapter: node({
-    mode: "standalone"
-  })
+  output: 'server',
+  adapter: netlify()
 });
